5a2c66ea278dace8204f66150cc050a902c13d3a
thom-d <d.thomas014@gmail.com>
2015-10-21 19:49:41 -0400

    mouse behavior and hadouken

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
-$(document).ready(function () {
-
-
-
-	
-});
+$('.ryu').mouseenter(function() {
+	$('.ryu_still').hide();
+	$('.ryu_ready').show();
+})
+.mouseleave(function() {
+	$('.ryu_still').show();
+	$('.ryu_ready').hide();
+})
+.mousedown(function() {
+	// play hadouken sounds
+	$('.ryu_ready').hide();
+	$('.ryu_throwing').show();
+	$('.hadouken').show();
+	// show hadouken and animate it to the right of the screen
+})
+.mouseup(function () {
+	$('.ryu_throwing').hide();
+	$('.ryu_ready').show();
+})
